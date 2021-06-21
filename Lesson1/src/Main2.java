f4e27577e809d47cc638970db3f0a2a9d7e3367f
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2021-06-21 19:17:04 +0300

    Lesson2: домашнее задание

--- Lesson1/src/Main2.java
+++ Lesson1/src/Main2.java
@@ -1,13 +1,8 @@
 public class Main2 {
     public static void main(String[] args) {
-        System.out.println("sakfj");
-
-        int i = 0;
-        int i2 = 0;
-        int i3 = 0;
-        System.out.println("sakfj");
-        System.out.println("sakfj");
-        System.out.println("sakfj");
-
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
