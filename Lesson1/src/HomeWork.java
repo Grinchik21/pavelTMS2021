01ee444d8dd2f45badf92877e1d34c796a346734
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2021-06-21 20:17:52 +0300

    хоме ворк(изменение файла)1

--- Lesson1/src/HomeWork.java
+++ Lesson1/src/HomeWork.java
@@ -1,5 +1,11 @@
 public class HomeWork {
     public static void main(String[] args) {
-              System.out.println("sdf");
+        System.out.println("sdf");
+        test();
+        int mm = 0;
+    }
+
+    static void test() {
+
     }
 }
