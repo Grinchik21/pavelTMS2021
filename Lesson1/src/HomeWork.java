f4910f7609a63af55e439cb43fe3edf84b057f59
Pavel Iliukevich <pavel.iliukevich@ontravelsolutions.com>
2021-06-24 09:48:34 +0300

    Лекция 1: домашка

--- Lesson1/src/HomeWork.java
+++ Lesson1/src/HomeWork.java
@@ -1,15 +1,27 @@
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println("sdf");
-        test();
-        int mm = 0;
-        int i = 2;
-        String hello = "hello";
-        String str = "llll";
-        char c = 'c';
+        int count = 0;
+        for (String arg : args) {
+            int i = Integer.parseInt(arg);
+            if (i > 0) {
+//                count++;
+                count = count + 1;
+            }
+        }
+        System.out.println(count);
+        //вариант со сканером
+//        Scanner scanner = new Scanner(System.in);
+//        String line = scanner.nextLine();
+//        System.out.println(line);
+//        scanner = new Scanner(line);
+//        while (scanner.hasNext()) {
+//            int value = scanner.nextInt();
+//            if (value > 0) {
+////                count++;
+//                count = count + 1;
+//            }
+//        }
+//        System.out.println(count);
     }
 
-    static void test() {
-
-    }
 }
